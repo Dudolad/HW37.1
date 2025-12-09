b900434bb814dd36a96b12b93e380d36ab99b74e
dudolad_yaroslav <dudolad2009@gmail.com>
2025-12-09 22:34:24 +0200

    main

--- HW37.1/HW37.1.cpp
+++ HW37.1/HW37.1.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include "Ingredient.h"
+#include <queue>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    priority_queue<Ingredient> kitchen;
+
+    kitchen.push(Ingredient("Rice", 20));
+    kitchen.push(Ingredient("Chicken", 30));
+    kitchen.push(Ingredient("Sauce", 5));
+    kitchen.push(Ingredient("Vegetables", 15));
+
+    cout << "Order of cooking:\n";
+
+    while (!kitchen.empty()) {
+        Ingredient ing = kitchen.top();
+        kitchen.pop();
+
+        cout << ing.getName() << " (cook time: " << ing.getTime() << " min)" << endl;
+    }
+
+    return 0;
 }
